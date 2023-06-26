906c26b67da4cfba6782731f4f1b76d1bbdb8e50
JP <joaopedrogeibdasilv@gmail.com>
2023-06-26 20:57:26 -0300

    marca com em

--- src/main/java/com/mercadoComMaven/service/MarcaService.java
+++ src/main/java/com/mercadoComMaven/service/MarcaService.java
@@ -8,33 +8,27 @@ import java.util.List;
 public class MarcaService {
 
     public static void criar(Marca objeto) {
-        MarcaDAO marcaDAO = new MarcaDAO();
-        marcaDAO.create(objeto);
+        MarcaDAO.getInstance().create(objeto);
     }
 
     public static Marca buscar(int codigo) {
-        MarcaDAO marcaDAO = new MarcaDAO();
-        return marcaDAO.retrive(codigo);
+        return MarcaDAO.getInstance().retrive(codigo);
     }
 
     public static Marca buscar(String descricao) {
-        MarcaDAO marcaDAO = new MarcaDAO();
-        return marcaDAO.retrive(descricao);    
+        return MarcaDAO.getInstance().retrive(descricao);    
     }
 
     public static List<Marca> buscar() {
-        MarcaDAO marcaDAO = new MarcaDAO();
-        return marcaDAO.retrive();   
+        return MarcaDAO.getInstance().retrive();   
     }
 
     public static void atualizar(Marca objeto) {
-        MarcaDAO marcaDAO = new MarcaDAO();
-        marcaDAO.update(objeto);   
+        MarcaDAO.getInstance().update(objeto);   
     }
 
     public static void excluir(Marca objeto) {
-        MarcaDAO marcaDAO = new MarcaDAO();
-        marcaDAO.delete(objeto);   
+        MarcaDAO.getInstance().delete(objeto);   
     }
     
 }
